f4860efb351f4e99d1e54ae31d6a053ff5c8fc87
brandonHahnPersonal <brandonneil1998@gmail.com>
2022-03-09 19:09:02 -0800

    added comment

--- HW3.py
+++ HW3.py
@@ -6,8 +6,8 @@
 #First create a new dictionary.
 #Iterate the input dictionary, adding the values to the new dictionary
 
-#inputs: dictionary of users
-#outputs: dictionary of tasks
+#inputs: dictionary of users with values as a dictionary of tasks
+#outputs: dictionary of tasks with a dictionary of users as values
 bTest = {'John': {'task1': 5}, 'Rae': {'task1': 10, 'task2': 4}, 'Kelly': {'task1': 8, 'task3': 5}, 'Alex': {'task1': 11, 'task2': 2, 'task3': 1}, 'Aaron': {'task2': 15}, 'Ethan':{'task3': 12}, 'Helen': {'task3': 10}}
 for a, b in bTest.items():
    for c, d in b.items():
@@ -17,7 +17,7 @@ def sprintLog (sprint):
    """This function takes a dictionary of users with associated hours, and returns a dictionary of tasks"""
    newDict = {}
 
-   for a, b in sprint.items():
-      for c, d in b.items():
+   for key, inputTuple in sprint.items(): #iterate through entire input dict
+      for tupleKey, tupleVal in tupleVal.items(): #iterate through the dict that is the value
          print(c,"what is going on", d)
 
