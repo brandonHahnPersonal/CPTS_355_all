18a5d7a33d2f53c5fecfb2a27df39d146a57e74d
brandonHahnPersonal <brandonneil1998@gmail.com>
2022-03-09 19:02:21 -0800

    trying to iterate through input

--- HW3.py
+++ HW3.py
@@ -3,11 +3,17 @@
 #possibly connected to github?
 
 #Create a funciton that takes a dictionary of users and returns a dictionary of tasks, where each task is associated with the users who worked on the task.
-#First divide keys and values.
-#
+#First create a new dictionary.
+#Iterate the input dictionary, adding the values to the new dictionary
+
 #inputs: dictionary of users
 #outputs: dictionary of tasks
 
 def sprintLog (sprint):
    """This function takes a dictionary of users with associated hours, and returns a dictionary of tasks"""
+   newDict = {}
+
+   for a, b in sprint.items():
+      for c, d in b.items():
+         print(c,"what is going on", d)
 
