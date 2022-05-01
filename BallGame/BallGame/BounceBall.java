34f2b7cfb30277a78d9b0d36da483a974dca677d
brandonHahnPersonal <brandonneil1998@gmail.com>
2022-04-30 18:31:53 -0700

    when a ball goes out. bounce ball fails.

--- BallGame/BallGame/BounceBall.java
+++ BallGame/BallGame/BounceBall.java
@@ -3,8 +3,6 @@ import java.awt.Color;
 public class BounceBall extends BasicBall
 {
     private double bounceCount;
-    protected double rx, ry;         // position
-    protected double vx, vy;         // velocity
 
     public BounceBall(double r, Color c)
     {
@@ -16,27 +14,34 @@ public class BounceBall extends BasicBall
         vy = StdRandom.uniform(-0.01, 0.01);
     }
 
-    // move the ball one step
-    public void move() {
-        
+    // move the ball one step   
+
+
+    public void move()
+    {        
         rx = rx + vx;
         ry = ry + vy;
-        if ((Math.abs(rx) > 1.0) || (Math.abs(ry) > 1.0)) 
+        if (Math.abs(rx) > 0.9)
         {
             bounceCount = bounceCount -1;
-            if (bounceCount >0)
+            if (bounceCount < 0)
             {
-                isOut = false;
-                System.out.println(isOut);
+                isOut = true;
             }
-            else 
+            vx = -vx;
+        }
+        else if (Math.abs(ry) > 0.9)
+        {            
+            bounceCount = bounceCount -1;
+            vy = -vy;
+            if (bounceCount < 0)
             {
-                
-                System.out.println("testtttttttttt");
                 isOut = true;
             }
         }
     }
+
+
 
 
     public int getScore() {
